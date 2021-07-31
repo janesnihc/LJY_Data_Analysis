86a63902f94506223d96e17712505abbe191980c
yangbob <yangbob@didiglobal.com>
2021-07-31 17:17:41 +0800

    update pandas_demo.py

--- demo/pandas_demo.py
+++ demo/pandas_demo.py
@@ -9,4 +9,11 @@ print(df.columns.tolist())
 print("行:", df.shape[0], "列:", df.shape[1])
 # 查看数据前5行
 print(df.head())
+# 单独查看wangpeng的数据
+print(df.loc[df["name"] == "wangpeng"])
+
+
+# 数据透视表部分
+# 查看每个人的得分（score）
+print(df.groupby("name")["score"].sum())
 
